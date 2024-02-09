35304792e494605a831e5ce0caf481045f8a2d7f
Manasi Mehta <psymm11@nottingham.ac.uk>
2024-02-09 10:57:46 +0000

    removed unneccessary files

--- eufs_tracks/eufs_tracks/converter_tool/__init__.py
+++
@@ -1,2 +0,0 @@
-from .converter import Converter  # noqa: F401
-from .converter_gui import EUFSConverterGUI  # noqa: F401
--- eufs_tracks/eufs_tracks/track_generator/__init__.py
+++
@@ -1,2 +0,0 @@
-from .track_generator import TrackGenerator  # noqa: F401
-from .track_generator_gui import EUFSTracksGUI  # noqa: F401
